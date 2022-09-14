f86c42b220153d2214b4ef3b8946e8d11336bf3a
devbeta8t3 <sehoan30@gmail.com>
2022-09-14 16:56:09 +0900

    9/14 Apartments - select option parts done

---
+++ Apartments/src/main/java/db/beans/QueryBean.java
@@ -0,0 +1,142 @@
+package db.beans;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class QueryBean {
+
+	Connection conn;
+	Statement stmt;
+	ResultSet rs;
+
+	public QueryBean() {
+		conn = null;
+		stmt = null;
+		rs = null;
+	}
+
+	public void getConnection() {
+		try {
+			conn = DBConnection.getConnection();
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		try {
+			stmt = conn.createStatement();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void closeConnection() {
+		if (stmt != null) {
+			try {
+				stmt.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		if (conn != null) {
+			try {
+				conn.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+
+//	public ArrayList getUserInfo(String user_id) throws Exception {
+//
+//		StringBuffer sb = new StringBuffer();
+//
+//		sb.append("SELECT");
+//		sb.append("   U_ID, U_NAME, U_PHONE, U_GRADE, WRITE_TIME ");
+//		sb.append(" FROM ");
+//		sb.append("   USER_INFO_SAMPLE");
+//		sb.append(" WHERE ");
+//		sb.append("   U_ID LIKE '%"+user_id+"%' ");
+//		sb.append(" ORDER BY");
+//		sb.append("     WRITE_TIME");
+//
+//		rs = stmt.executeQuery(sb.toString());
+//
+//		ArrayList res = new ArrayList();
+//		while (rs.next()) {
+//
+//			res.add(rs.getString(1));
+//			res.add(rs.getString(2));
+//			res.add(rs.getString(3));
+//			res.add(rs.getString(4));
+//			res.add(rs.getString(5));
+//			
+//		}
+//		System.out.println(sb.toString());
+//		return res;
+//	}
+	
+	/**
+	 * 선택된 도이름으로 시군구 이름을 ArrayList res에 저장하는 메소드 
+	 * @param doName : select id="doSelect"에서 선택된 value
+	 * @return (ArrayList) res : 시군구 리스트
+	 * @throws Exception
+	 */
+	public ArrayList getSiName(String doName) throws Exception {
+
+		StringBuffer sb = new StringBuffer();
+		System.out.println("넘어온 doName 값 : " +doName);	
+		sb.append("SELECT");
+		sb.append("   DISTINCT 시군구명 ");
+		sb.append(" FROM ");
+		sb.append("   dongcode ");	//테이블명
+		sb.append(" WHERE ");
+		sb.append("   시도명 = '"+doName+"' AND 시군구명 IS NOT NULL ");
+		sb.append(" ORDER BY");
+		sb.append("   시군구명");
+
+		rs = stmt.executeQuery(sb.toString());	// Query 실행
+
+		ArrayList res = new ArrayList();
+		while (rs.next()) {
+			res.add(rs.getString(1));
+			//res.add(rs.getString(2));
+		}
+		
+		System.out.println(sb.toString());	// 실행된 쿼리문 출력
+		System.out.println(res.toString());	// 실행된 쿼리문 출력
+		return res;
+	}
+
+	/**
+	 * 선택된 시군구 이름으로 읍면동 이름을 ArrayList res에 저장하는 메소드 
+	 * @param doName
+	 * @return ArrayList res
+	 * @throws Exception
+	 */
+	public ArrayList getDongName(String siName, String doName) throws Exception {
+
+		StringBuffer sb = new StringBuffer();
+
+		sb.append("SELECT");
+		sb.append("   DISTINCT 법정동코드, 읍면동명 ");
+		sb.append(" FROM ");
+		sb.append("   dongcode ");	//테이블명
+		sb.append(" WHERE ");
+		sb.append("   시군구명 = '"+siName+"' AND 시도명 = '"+doName+"' AND 읍면동명 IS NOT NULL ");
+		sb.append(" ORDER BY");
+		sb.append("   읍면동명");
+
+		rs = stmt.executeQuery(sb.toString());	// Query 실행
+
+		ArrayList res = new ArrayList();
+		while (rs.next()) {
+			res.add(rs.getString(1));
+			res.add(rs.getString(2));
+		}
+		
+		System.out.println(sb.toString());	// 실행된 쿼리문 출력
+		System.out.println(res.toString());	// 실행된 쿼리문 출력
+		return res;
+	}
+}
---
+++ Apartments/src/main/webapp/dongDB.jsp
@@ -0,0 +1,53 @@
+<%@page import="java.sql.SQLException"%>
+<%@page import="java.util.ArrayList"%>
+<%@ page contentType="text/html; charset=EUC-KR" pageEncoding="EUC-KR"%>
+<jsp:useBean id="QueryBean" scope="page" class="db.beans.QueryBean" />
+<jsp:setProperty property="*" name="QueryBean" />
+<%
+	response.setHeader("Cache-Control", "no-store");
+	response.setHeader("pragma", "no-cache");
+	response.setDateHeader("Expires", 0);
+	request.setCharacterEncoding("UTF-8");
+		
+	String siName = (request.getParameter("시군구명") == null) ? "" : request.getParameter("시군구명").trim();
+	String doName = (request.getParameter("시도명") == null) ? "" : request.getParameter("시도명").trim();
+	QueryBean.getConnection();
+	ArrayList resArr = new ArrayList();
+	try {
+		resArr = QueryBean.getDongName(siName, doName);
+		
+	} catch (SQLException e) {
+		out.print(e.toString());
+	} finally {
+		QueryBean.closeConnection(); 
+	}
+	out.println("{");
+	out.println("\"datas\":[");
+	
+	if (resArr.size() == 0) {
+		out.println("]");
+		out.println("}");
+	} else {
+		out.print("{");
+		out.print("\"법정동코드\":\"" + (String) resArr.get(0) + "\", ");
+		out.print("\"읍면동명\":\"" + (String) resArr.get(1) + "\" ");
+//		out.print("\"NAME\":\"" + (String) resArr.get(1) + "\", ");
+//		out.print("\"PHONE\":\"" + (String) resArr.get(2) + "\", ");
+//		out.print("\"GRADE\":\"" + (String) resArr.get(3) + "\", ");
+//		out.print("\"WRITE_TIME\":\"" + (String) resArr.get(4) + "\" ");
+		out.print("}");
+		for (int i = 2; i < resArr.size(); i=i+2) {
+			out.print(",");
+			out.print("{");
+			out.print("\"법정동코드\":\"" + (String) resArr.get(i) + "\", ");
+			out.print("\"읍면동명\":\"" + (String) resArr.get(i + 1) + "\" ");
+//			out.print("\"NAME\":\"" + (String) resArr.get(i + 1) + "\", ");
+//			out.print("\"PHONE\":\"" + (String) resArr.get(i + 2) + "\", ");
+//			out.print("\"GRADE\":\"" + (String) resArr.get(i + 3) + "\", ");
+//			out.print("\"WRITE_TIME\":\"" + (String) resArr.get(i + 4) + "\" ");
+			out.print("}");
+		}
+		out.println("]");
+		out.println("}");
+	}
+%>
